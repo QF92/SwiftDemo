13f50fa4797cbb564e0261c9cbbacbd93046e8e1
Flash_qing <1026161046@qq.com>
2020-09-18 16:38:31 +0800

    定位使用

--- swiftdemo/swiftdemo/BaseViewController/VC/MainVC/LoginVC.swift
+++ swiftdemo/swiftdemo/BaseViewController/VC/MainVC/LoginVC.swift
@@ -11,6 +11,7 @@ import RxCocoa
 import RxSwift
 
 
+import CoreLocation  //导入定位核心库
 class Person: NSObject {
 
     fileprivate var name:String = "man"
@@ -34,7 +35,8 @@ class LoginVC: BaseViewController {
         print(p.name)
 //        print(p.age)
         //编译器不会提示age属性 如果强行写p.age,会报错'age' is inaccessible due to 'private' protection level
-       
+        //开启定位
+        loadLocation()
         
        self.view.addSubview(self.usernameOutlet)
        self.view.addSubview(self.usernameValidOutlet)
@@ -186,4 +188,119 @@ class LoginVC: BaseViewController {
     }
     */
 
+       var locationManager = CLLocationManager()
+       var currentLocation:CLLocation!
+
+       var lock = NSLock()
+}
+
+
+extension LoginVC: CLLocationManagerDelegate{  //遵守定位代理，以获取定位结果。
+  
+     //打开定位
+     func loadLocation(){
+ 
+        locationManager.delegate = self
+         //定位方式
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest  //定位精确度（最高）一般有电源接入，比较耗电
+                                        //kCLLocationAccuracyNearestTenMeters; //精确到10米
+        
+        
+     //iOS8.0以上才可以使用
+        if(UIDevice.current.systemVersion >= "8.0"){
+           //始终允许访问位置信息 //始终授权
+           locationManager.requestAlwaysAuthorization()
+           //使用应用程序期间允许访问位置数据   //弹出用户授权对话框，使用程序期间授权（ios8后)
+           locationManager.requestWhenInUseAuthorization()
+       }
+    //开启定位
+       locationManager.startUpdatingLocation()
+   }
+ 
+ 
+ 
+//获取定位信息
+  private func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+ 
+     lock.lock()
+    //取得locations数组的最后一个
+    let location:CLLocation = locations[locations.count-1]
+        currentLocation = locations.last   //注意：获取集合中最后一个位置（最新的位置）
+        //判断是否为空
+    
+    print("定位经纬度为：\(currentLocation.coordinate.latitude)")
+    
+    //一直发生定位错误输出结果为0：原因是我输出的是currentLocation.altitude(表示高度的)而不是currentLoction.coordinate.latitude（这个才是纬度）
+      if(location.horizontalAccuracy > 0){
+        let  lat = Double(String(format: "%.1f", location.coordinate.latitude))
+        let  long = Double(String(format: "%.1f", location.coordinate.longitude))
+          print("纬度:\(long!)")
+          print("经度:\(lat!)")
+          LonLatToCity()
+          //停止定位
+          locationManager.stopUpdatingLocation()
+         lock.unlock()
+    }
+ 
+}
+ 
+//出现错误
+    private func locationManager(manager: CLLocationManager, didFinishDeferredUpdatesWithError error: NSError?) {
+        print(error as Any)
+}
+ 
+///将经纬度转换为城市名
+func LonLatToCity() {
+    let geocoder: CLGeocoder = CLGeocoder()
+    geocoder.reverseGeocodeLocation(currentLocation) { (placemark, error) -> Void in
+ 
+        if(error == nil)
+        {
+            let array = placemark! as NSArray
+            let mark = array.firstObject as! CLPlacemark
+//            //城市
+//            var city: String = (mark.addressDictionary! as NSDictionary).value(forKey: "City") as! String
+//            //国家
+//            let country: NSString = (mark.addressDictionary! as NSDictionary).value(forKey: "Country") as! NSString
+//            //国家编码
+//            let CountryCode: NSString = (mark.addressDictionary! as NSDictionary).value(forKey: "CountryCode") as! NSString
+//            //街道位置
+//            let FormattedAddressLines: NSString = ((mark.addressDictionary! as NSDictionary).value(forKey: "FormattedAddressLines") as AnyObject).firstObject as! NSString
+//            //具体位置
+//            let Name: NSString = (mark.addressDictionary! as NSDictionary).value(forKey: "Name") as! NSString
+//            //省
+//            var State: String = (mark.addressDictionary! as NSDictionary).value(forKey: "State") as! String
+//            //区
+//            let SubLocality: NSString = (mark.addressDictionary! as NSDictionary).value(forKey: "SubLocality") as! NSString
+//
+//            //如果需要去掉“市”和“省”字眼
+//
+//            State = State.replacingOccurrences(of: "省", with: "")
+//            let citynameStr = city.replacingOccurrences(of: "市", with: "")
+            
+            
+            var street = mark.thoroughfare! // addressDictionary!["Street"] as? String ?? " "
+            //城市
+            let citys =  mark.subAdministrativeArea! // addressDictionary!["City"] as? String ?? " "
+            let state = mark.administrativeArea!//addressDictionary!["State"] as? String ?? " "
+            let zip =  mark.isoCountryCode!// addressDictionary!["ZIP"] as? String ?? " "
+            //国家
+            let countrys = mark.country! // addressDictionary!["Country"] as? String ?? " "
+            //国家编码
+            let countrycodes = mark.isoCountryCode! // addressDictionary!["Country"] as? String ?? " "
+            
+           
+ 
+ 
+ 
+        }
+        else
+        {
+            print(error)
+        }
+    }
+ 
+ 
+}
+ 
 }
