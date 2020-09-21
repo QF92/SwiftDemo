6f16e604e78ee505059ff805333891c8c0a9389b
Flash_qing <1026161046@qq.com>
2020-09-21 17:54:54 +0800

    Xzz

--- swiftdemo/swiftdemo/Utils/Extension/UIViewExtension.swift
+++ swiftdemo/swiftdemo/Utils/Extension/UIViewExtension.swift
@@ -152,4 +152,84 @@ public extension UIView {
         self.layer.mask = maskLayer
         return self
     }
+    
+    /// x origin of view.
+    var x: CGFloat {
+        get {
+            return frame.origin.x
+        }
+        set {
+            frame.origin.x = newValue
+        }
+    }
+    
+    /// y origin of view.
+    var y: CGFloat {
+        get {
+            return frame.origin.y
+        }
+        set {
+            frame.origin.y = newValue
+        }
+    }
+    
+    /// Width of view.
+    var width: CGFloat {
+        get {
+            return frame.size.width
+        }
+        set {
+            frame.size.width = newValue
+        }
+    }
+    
+    /// Height of view.
+    var height: CGFloat {
+        get {
+            return frame.size.height
+        }
+        set {
+            frame.size.height = newValue
+        }
+    }
+    
+    /// Size of view.
+    var size: CGSize {
+        get {
+            return frame.size
+        }
+        set {
+            frame.size = newValue
+        }
+    }
+    
+    /// Origin of view.
+    var origin: CGPoint {
+        get {
+            return frame.origin
+        }
+        set {
+            frame.origin = newValue
+        }
+    }
+    
+    /// CenterX of view.
+    var centerX: CGFloat {
+        get {
+            return center.x
+        }
+        set {
+            center.x = newValue
+        }
+    }
+    
+    /// CenterY of view.
+    var centerY: CGFloat {
+        get {
+            return center.y
+        }
+        set {
+            center.y = newValue
+        }
+    }
 }
